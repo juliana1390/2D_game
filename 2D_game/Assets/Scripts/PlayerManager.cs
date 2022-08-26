232e74c025713ab417f73620c58499cc5ea6ca71
juliana1390 <90989716+juliana1390@users.noreply.github.com>
2022-08-26 17:33:24 -0300

    jump / death of the player

--- 2D_game/Assets/Scripts/PlayerManager.cs
+++ 2D_game/Assets/Scripts/PlayerManager.cs
@@ -8,18 +8,54 @@ public class PlayerManager : MonoBehaviour
 
     private float movePlayer;
 
-    public float speed;
+    public float speed, jumpForce;
+    private bool jump, isGrounded, restartPlayer;
+    private GameObject cameraPos, initialPos;
     
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        cameraPos = GameObject.Find("Main Camera");
+        initialPos = GameObject.Find("initialPos");
     }
 
     // Update is called once per frame
     void Update()
     {
+        print(restartPlayer);
         movePlayer = Input.GetAxis("Horizontal");
+        jump = Input.GetButtonDown("Jump");
         player.velocity = new Vector2(movePlayer*speed, player.velocity.y);
+        if (jump == true && isGrounded == true)
+        {
+            player.AddForce(new Vector2(0, jumpForce));
+            isGrounded = false;
+        }
+        Restart();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) // it avoids several jumps in the air before touching the ground
+    {
+        if (col.gameObject.layer == 8)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("armadilhas") == true)
+        {
+            restartPlayer = true;
+        }
+    }
+    private void Restart()
+    {
+        if (restartPlayer == true)
+        {
+            player.transform.position = new Vector2(initialPos.transform.position.x, initialPos.transform.position.y);
+            restartPlayer = false;
+        }
     }
 }
