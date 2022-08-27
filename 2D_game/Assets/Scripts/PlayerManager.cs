71974daa5fa03eccb41136b1f2b843521fae4661
juliana1390 <90989716+juliana1390@users.noreply.github.com>
2022-08-27 11:20:29 -0300

    victory panel / level 2
    - conclusion of level 1
    - movement of victory panel
    - creation of level 2 scene

--- 2D_game/Assets/Scripts/PlayerManager.cs
+++ 2D_game/Assets/Scripts/PlayerManager.cs
@@ -8,9 +8,10 @@ public class PlayerManager : MonoBehaviour
 
     private float movePlayer;
 
-    public float speed, jumpForce;
-    private bool jump, isGrounded, restartPlayer;
+    public float speed, jumpForce, cameraHeight, winSpeed;
+    private bool jump, isGrounded, restartPlayer, win;
     private GameObject cameraPos, initialPos;
+    public GameObject winPanel;
     
     // Start is called before the first frame update
     void Start()
@@ -18,21 +19,25 @@ public class PlayerManager : MonoBehaviour
         player = GetComponent<Rigidbody2D>();
         cameraPos = GameObject.Find("Main Camera");
         initialPos = GameObject.Find("initialPos");
+        win = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(restartPlayer);
+        print(win);
         movePlayer = Input.GetAxis("Horizontal");
         jump = Input.GetButtonDown("Jump");
+        cameraPos.transform.position = new Vector3(cameraPos.transform.position.x, player.transform.position.y + cameraHeight, cameraPos.transform.position.z);
         player.velocity = new Vector2(movePlayer*speed, player.velocity.y);
         if (jump == true && isGrounded == true)
         {
             player.AddForce(new Vector2(0, jumpForce));
             isGrounded = false;
         }
+
         Restart();
+        winGame();
     }
 
     private void OnCollisionEnter2D(Collision2D col) // it avoids several jumps in the air before touching the ground
@@ -49,6 +54,11 @@ public class PlayerManager : MonoBehaviour
         {
             restartPlayer = true;
         }
+
+        if (col.CompareTag("win") == true)
+        {
+            win = true;
+        }
     }
     private void Restart()
     {
@@ -56,6 +66,15 @@ public class PlayerManager : MonoBehaviour
         {
             player.transform.position = new Vector2(initialPos.transform.position.x, initialPos.transform.position.y);
             restartPlayer = false;
+        }
+    }
+
+    private void winGame()
+    {
+        if (win == true)
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+            winPanel.transform.position = Vector2.MoveTowards(winPanel.transform.position, cameraPos.transform.position, winSpeed * Time.deltaTime);
         }
     }
 }
